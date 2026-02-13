03defae536e774aeaec815d1963846d52e2b09ef
Harpreet Singh <harpreet.singh7@uleth.ca>
2026-02-12 18:05:12 -0700

    Cleanup: Remove Firebase placeholders

--- script.js
+++ script.js
@@ -214,16 +214,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // --- FIREBASE INTEGRATION ---
-    // TODO: Replace with your actual Firebase project configuration
-    // Get this from: https://console.firebase.google.com/
-    // const firebaseConfig = {
-    //     apiKey: "YOUR_API_KEY_HERE",
-    //     authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    //     projectId: "YOUR_PROJECT_ID",
-    //     storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    //     messagingSenderId: "YOUR_SENDER_ID",
-    //     appId: "YOUR_APP_ID"
-    // };
 
     const firebaseConfig = {
         apiKey: "AIzaSyBmpMxcF7EHjeFKS-IrEWij8sDnQsngRSQ",
